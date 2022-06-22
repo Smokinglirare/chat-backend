83defc85e30bb6983c5d4776cb806f9224de0cca
Sebastian <sebastian.thorneus@gmail.com>
2022-06-22 02:54:50 +0100

    nu kan man ta emot meddelanden från clienten

--- server.js
+++ server.js
@@ -2,11 +2,34 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+
 const port = 4000;
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello World</h1>");
 });
+
+io.on("connection", (socket) => {
+    console.log("En användare har anslutit");
+
+    socket.on("disconnect", () => {
+        console.log("En användare har lämnat")
+    })
+
+    socket.on("chat message", (data) => {
+        console.log(`message : ${data}`)
+})
+
+})
 
 server.listen(port, () => {
     console.log(`Servern körs på port ${port}.`)
