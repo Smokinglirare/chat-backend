6e8a2bc05e92029030e35d58ac1e429b69c3d342
Sebastian Torneus <Sebastian.thorneus@gmail.com>
2022-06-23 21:20:42 +0200

    lite rum osv

--- server.js
+++ server.js
@@ -8,28 +8,55 @@ const { Server } = require("socket.io");
 const port = 4000;
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
+const rooms = {
+  default: {
+    name: "Default room"
+  },
+};
 
 
 io.on("connection", (socket) => {
-    console.log("En användare har anslutit");
+  console.log( `${socket.id} har anslutit`);
 
-    socket.on("disconnect", () => {
-        console.log("En användare har lämnat")
-    })
-    socket.on("chat message", (data) => {
-        console.log(`message : ${data}`)
-        socket.broadcast.emit("message received", data);
-})
+  socket.on("setUsername", (name) => {
+    socket.username = name,
+    console.log(socket.username)
+  })
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} har lämnat servern pga ${reason}`)
+    console.log(socket.username)
+  })
+  socket.on("chat message", (data) => {
+    console.log(` ${socket.id} message : ${data}`)
+    socket.to(data.roomName).emit("message received", data);
+  })
+  socket.on("create room", (room) => {
+    rooms[room] = {
+      name: room
+    }
+    console.log(rooms);
+  })
 
+
+  socket.on("join_room", (room) => {
+    console.log(`${socket.id} har anslutit till ${room}`)
+    socket.join(room);
+    io.to(room).emit("joined_room", socket.id);
+    console.log(socket.rooms)
+  })
+  socket.on("delete_room", (room) => {
+    io.in(room).socketsLeave("room");
+  })
+ 
 
 })
 
 server.listen(port, () => {
-    console.log(`Servern körs på port ${port}.`)
+  console.log(`Servern körs på port ${port}.`)
 });
